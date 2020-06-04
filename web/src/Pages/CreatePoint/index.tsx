ac796a59b98d8a9108db56a6716eece28f66445a
Paulo Pinheiro <pauloprap57@gmail.com>
2020-06-04 18:05:19 -0300

    Criando a estrutura inicial do projeto

---
+++ web/src/Pages/CreatePoint/index.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { FiArrowLeft } from "react-icons/fi";
+import { Link } from "react-router-dom";
+
+import "./styles.css";
+
+import logo from "../../assets/logo.svg";
+
+export default function CreatePoint() {
+  return (
+    <div id="page-create-point">
+      <header>
+        <img src={logo} alt="Ecoleta" />
+        <Link to="/">
+          <FiArrowLeft />
+          Voltar para Home
+        </Link>
+      </header>
+
+      <form>
+        <h1>Cadastro do ponto de coleta</h1>
+        <fieldset>
+          <legend>
+            <h2>Dados</h2>
+          </legend>
+
+          <div className="field">
+            <label htmlFor="name">Nome da entidade</label>
+            <input type="text" name="name" id="name" />
+          </div>
+
+          <div className="field-group">
+            <div className="field">
+              <label htmlFor="email">E-mail</label>
+              <input type="email" name="email" id="email" />
+            </div>
+
+            <div className="field">
+              <label htmlFor="whatsapp">Whatsapp</label>
+              <input type="text" name="whatsapp" id="whatsapp" />
+            </div>
+          </div>
+        </fieldset>
+
+        <fieldset>
+          <legend>
+            <h2>Endereço</h2>
+            <span>Selecione o endereço no mapa</span>
+          </legend>
+        </fieldset>
+        <fieldset>
+          <legend>
+            <h2>Ítens de coleta</h2>
+            <span>Selecione um ou mais ítens abaixo</span>
+          </legend>
+        </fieldset>
+      </form>
+    </div>
+  );
+}
